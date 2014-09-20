3fb8085f880081fa2b5c221518079815912356bd
realiwa <iwamoto@pu-toyama.ac.jp>
2014-09-20 22:11:32 +0900

    edit

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'nantobudget';
+OpenSpending.identifier = 'nanto_budget';
 OpenSpending.year = '2014';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
